1a56fd127f515c484912b1a79ee863bec0201a88
fridask <fridask@stud.ntnu.no>
2019-03-12 12:12:13 +0100

    Update task_8.py

--- Part1/src/task_8.py
+++ Part1/src/task_8.py
@@ -3,10 +3,13 @@ from pyspark import SparkConf
 from pyspark.context import SparkContext
 
 sc = SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
+
 #Loading the albums dataset
 albums = sc.textFile("/datasets/albums.csv").map(lambda line: line.split(","))
+
 #finding the albums with a MTV critics of 5
 album_filtered=albums.filter(lambda x: x[8]=="5")
+
 #Transforming album_filtered into a RDD with only artist ID and the MTV critic
 album_mtvCritic = album_filtered.map(lambda x: (int(x[1]),(x[8])))
 
@@ -18,5 +21,6 @@ artistName=artists.map(lambda x: (int(x[0]), (x[2])) if x[2]!="" else (int(x[0])
 
 #joining album_mtvCritic and artistName on artist ID. Then we only take the distinct artists and sorting in alphabetical order
 album_artist_join = album_mtvCritic.join(artistName).map(lambda x: x[1][1]).distinct().sortBy(lambda x: x, True)
+
 #saving to text file
 album_artist_join.coalesce(1).saveAsTextFile("/Output/result_8")
