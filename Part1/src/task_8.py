6a937d6059294c401a91b6da5cf7564649336cf6
Harald Aarskog <44364067+haraldaarskog@users.noreply.github.com>
2019-03-12 12:09:57 +0100

    Update task_8.py

--- Part1/src/task_8.py
+++ Part1/src/task_8.py
@@ -4,14 +4,14 @@ from pyspark.context import SparkContext
 
 sc = SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
 #Loading the albums dataset
-albums = sc.textFile("/Users/haraldaarskog/Google\ Drive/Big\ data-arkitektur/Prosjekt/datasets/albums.csv").map(lambda line: line.split(","))
+albums = sc.textFile("/datasets/albums.csv").map(lambda line: line.split(","))
 #finding the albums with a MTV critics of 5
 album_filtered=albums.filter(lambda x: x[8]=="5")
 #Transforming album_filtered into a RDD with only artist ID and the MTV critic
 album_mtvCritic = album_filtered.map(lambda x: (int(x[1]),(x[8])))
 
 #loading the artists dataset
-artists = sc.textFile("/Users/haraldaarskog/Google\ Drive/Big\ data-arkitektur/Prosjekt/datasets/artists.csv").map(lambda line: line.split(","))
+artists = sc.textFile("/datasets/artists.csv").map(lambda line: line.split(","))
 
 #Transforming the artists RDD into a RDD with only artist ID and artist name. If the artistName is "" we replace it with the real name
 artistName=artists.map(lambda x: (int(x[0]), (x[2])) if x[2]!="" else (int(x[0]), (x[1])))
@@ -19,4 +19,4 @@ artistName=artists.map(lambda x: (int(x[0]), (x[2])) if x[2]!="" else (int(x[0])
 #joining album_mtvCritic and artistName on artist ID. Then we only take the distinct artists and sorting in alphabetical order
 album_artist_join = album_mtvCritic.join(artistName).map(lambda x: x[1][1]).distinct().sortBy(lambda x: x, True)
 #saving to text file
-album_artist_join.coalesce(1).saveAsTextFile("/Users/haraldaarskog/Google Drive/Workspace/git/BigDataGit/Part1/Output/result_8")
+album_artist_join.coalesce(1).saveAsTextFile("/Output/result_8")
