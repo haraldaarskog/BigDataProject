bbe77a846a6bcbba067579a3dd634b33d2daceba
fridask <fridask@stud.ntnu.no>
2019-03-12 12:05:28 +0100

    Update task_6.py

--- Part1/src/task_6.py
+++ Part1/src/task_6.py
@@ -9,8 +9,10 @@ albums = sc.textFile("/datasets/albums.csv").map(lambda line: line.split(","))
 
 #Calculating the average critic for each album ID
 album_critic = albums.map(lambda x: ((x[0]), (float(x[7])+float(x[8])+float(x[9]))/3))
+
 #sorting on critics
 album_critic_sorted = album_critic.sortBy(lambda x: x[1], False)
+
 #Taking out the top 10 albums with highest average critic and transforming it into a RDD
 album_top10=sc.parallelize(album_critic_sorted.take(10)).map(lambda x: x[0]+"\t"+str(x[1]))
 
