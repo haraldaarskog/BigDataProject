930dfe6d70bfd5a34306657332475a5d25146910
Harald Aarskog <44364067+haraldaarskog@users.noreply.github.com>
2019-03-23 14:39:19 +0100

    Update recommend.py

--- Part2/recommend.py
+++ Part2/recommend.py
@@ -15,19 +15,18 @@ def recommend(user, k, file, output):
     tweets2=tweets.map(lambda line: line.split("\t")).flatMapValues(lambda x: x.split(' ')).map(lambda x: (x[0],(x[1])))
     #flatMapping the values and splitting on space, remapping to key-value pair
 
-    #finding alle the tweets for our input user
-    userTweet1=tweets2.lookup(user)
-    #saving the tweet as a dictionary and count
-    userTweet2= sc.parallelize(userTweet1).countByValue()
-
     #filtering out the input user and tweets not in the tweets of the user. Then mapping in key-value pair: ((user, word), 1)
-    corp777=tweets2.filter(lambda x: x[0]!=user and x[1] in userTweet1).map(lambda x: ((x[0],x[1]),1))
+    corp777=tweets2.map(lambda x: ((x[0],x[1]),1))
+
+    userTweet=corp777.filter(lambda x: x[0][0]==user).reduceByKey(lambda n,m:n+m).map(lambda x: (x[0][1],x[1])).collectAsMap()
+
+    corp100=corp777.filter(lambda x: x[0][0]!=user and x[0][1] in userTweet)
 
     #reducing
-    corp4=corp777.reduceByKey(lambda n,m: n+m)
+    corp4=corp100.reduceByKey(lambda n,m: n+m)
 
     #finding the minimum frequency
-    corp5=corp4.map(lambda x: (x[0][0], min(userTweet2[x[0][1]],x[1])))
+    corp5=corp4.map(lambda x: (x[0][0], min(userTweet[x[0][1]],x[1])))
 
 
     corp6=corp5.reduceByKey(lambda n,m: n+m)
@@ -37,9 +36,7 @@ def recommend(user, k, file, output):
     xx=x.filter(lambda key : key[1] < k and key[1] >=0)
     xxx=xx.map(lambda x: x[0][0]+"\t"+str(x[0][1]))
     xxx.coalesce(1).saveAsTextFile(output)
-    
-  #  x = sc.parallelize(compare2.take(k)).map(lambda x: x[0]+"\t"+str(x[1]))
 
-recommend("rachele_m13", 5, "tweets.tsv", "output.tsv")
+recommend("rachele_m13", 5, "tweets.tsv", "output44.tsv")
 end = time.time()
 print(end - start)
